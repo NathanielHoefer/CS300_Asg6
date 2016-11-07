0051cda20f8f2daf771d513c8f7427b7639f9a32
Nathaniel <nathaniel.d.hoefer@gmail.com>
2016-11-07 16:31:46 -0600

    Add contact info in menu working correctly

--- main.cpp
+++ main.cpp
@@ -26,9 +26,96 @@ int main()
 {
 
 
-
+	bool isFinished = false;
+	int selection = 0;
 
 	BSTree tree;
+
+	while (!isFinished)
+	{
+
+		cout << "Binary Search Tree Application - Program 6" << endl;
+		cout << "Please make a selection" << endl << endl;
+
+		cout << "1. Add Contact Information" << endl;
+		cout << "2. Delete a Telephone Number" << endl;
+		cout << "3. Locate a Telephone Number" << endl;
+		cout << "4. Traverse List: In-order" << endl;
+		cout << "5. Traverse List: Pre-order" << endl;
+		cout << "6. Traverse List: Post-order" << endl;
+		cout << "7. Quit" << endl;
+
+
+		cin >> selection;
+
+		if ( selection > 0 && selection <= 7)
+		{
+			switch (selection) {
+				// Add contact
+				case 1:
+				{
+					ContactInfo info;
+
+					cout << "Enter Phone Number: " << endl;
+					cin >> info.mPhone;
+					cout << "Enter Name: " << endl;
+					cin >> info.mName;
+					cout << "Enter Address: " << endl;
+					cin >> info.mAddress;
+					cout << "Ender Email: " << endl;
+					cin >> info.mEmail;
+
+					tree.insert(info);
+
+					break;
+				}
+
+				// Delete contact
+				case 2:
+				{
+					break;
+				}
+
+
+				// Locate contact
+				case 3:
+				{
+					break;
+				}
+
+				// Traverse in-order
+				case 4:
+				{
+					break;
+				}
+
+				// Traverse pre-order
+				case 5:
+				{
+					break;
+				}
+
+				// Traverse post-order
+				case 6:
+				{
+					break;
+				}
+
+				// Quit and save
+				case 7:
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			cout << "Invalid response" << endl;
+		}
+
+	}
+
+
 
 	ContactInfo c1 = {99, "one", "116 St", "1@hotmail.com"};
 	ContactInfo c2 = {88, "two", "Car St", "2@hotmail.com"};
